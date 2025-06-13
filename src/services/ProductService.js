a9f691a644a7d13875b4c70e92c27c9628bfa13d
DMRSCK DIvisekara <10charithyaskd@gmail.com>
2025-06-13 09:39:35 +0530

    edit

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -3,13 +3,6 @@ const ProductService = {
   getProducts: async () => {
     // Return mock data or empty array for now
     return [];
-  },
-   getFeaturedProducts: async () => {
-    return[];
-},
-getNewArrivals: async () => {
-    return [];
   }
-};
-
+}
 export default ProductService;
